f6fad36f9826e2fe4ac11ee992aeeb538ea3ad7c
write-meetkakadiya <write.meetkakadiya@gmail.com>
2026-02-02 21:17:39 -0600

    Update index.js
    Remove the comments

--- index.js
+++ index.js
@@ -6,31 +6,13 @@ const { json } = require('body-parser');
 // const translate = new Translate();
 // Creates a client
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: 'APIkey.json'
+    keyFilename: '***'
 });
-
-// Performs label detection on the image file
-// client
-//     .labelDetection('./demo.png')
-//     .then(results => {
-//         const labels = results[0].labelAnnotations;
-
-//         console.log('Labels:');
-//         labels.forEach(label => console.log(label.description));
-//         //console.log(results);
-//     })
-//     .catch(err => {
-//         console.error('ERROR:', err);
-//     });
 
 // Image to text
 app.use('/test', async function () {
-    // const [result] = await client.textDetection('./demo.png');
-    const [result] = await client.textDetection('./demo1.jpg');
+    const [result] = await client.textDetection('./demo.jpg');
     const detections = result.textAnnotations;
-    console.log('Text:');
-    console.log('-----------DATA-----------', detections[0].description)
-    // detections.forEach(text => console.log(text[0].description));
 })
 
 // PDF to text
@@ -38,9 +20,6 @@ app.use('/test', async function () {
 app.use('/text', async function () {
     const gcsSourceUri = `gs://test710/sample1.pdf`;
     const gcsDestinationUri = `gs://test710/output/`;
-
-    // const gcsSourceUri = `./sample.pdf`;
-    // const gcsDestinationUri = `./`;
 
     const inputConfig = {
         // Supported mime_types are: 'application/pdf' and 'image/tiff'
